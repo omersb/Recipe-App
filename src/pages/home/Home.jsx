92400e7be43e85241e4512c9f473b1c1677e1e58
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-15 23:56:44 +0300

    app

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,7 +14,8 @@ const Home = () => {
   const APP_ID = process.env.REACT_APP_APP_ID;
   const APP_KEY = process.env.REACT_APP_APP_KEY;
 
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+  
 
   const getData = async () => {
     if (query) {
