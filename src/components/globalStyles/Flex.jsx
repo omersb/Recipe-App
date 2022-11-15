d824fb7044826a6349b69e43af457175b04240e3
omersb <omersaidbuldukk@gmail.com>
2022-11-15 13:50:43 +0300

    update

--- src/components/globalStyles/Flex.jsx
+++ src/components/globalStyles/Flex.jsx
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Flex = styled.div`
-  display: flex;
-  justify-content: ${({ justify }) => (justify ? justify : 'center')};
-  align-items: ${({ align }) => (align ? align : 'center')};
-  flex-wrap: ${({ wrap }) => wrap && wrap};
+	display: flex;
+	justify-content: ${({ justify }) => (justify ? justify : "center")};
+	align-items: ${({ align }) => (align ? align : "center")};
+	flex-wrap: ${({ wrap }) => wrap && wrap};
 `;
 
 export default Flex;
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,31 +1,31 @@
-import Nav, { Logo, Menu, MenuLink, Hamburger } from './Navbar.style';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { useState } from 'react';
+import Nav, { Logo, Menu, MenuLink, Hamburger } from "./Navbar.style";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <Nav justify="space-between" wrap="wrap">
-      <Logo to="/">
-        <i>{'<Clarusway/>'}</i>
-        <span>Recipe</span>
-      </Logo>
+	return (
+		<Nav justify="space-between" wrap="wrap">
+			<Logo to="/">
+				<i>{"<Clarusway/>"}</i>
+				<span>Recipe</span>
+			</Logo>
 
-      <Hamburger onClick={() => setIsOpen(!isOpen)}>
-        <GiHamburgerMenu />
-      </Hamburger>
+			<Hamburger onClick={() => setIsOpen(!isOpen)}>
+				<GiHamburgerMenu />
+			</Hamburger>
 
-      <Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
-        <MenuLink to="/">Home</MenuLink>
-        <MenuLink to="about">About</MenuLink>
-        <MenuLink to="register">Register</MenuLink>
-        <MenuLink to="login" onClick={() => sessionStorage.clear()}>
-          Logout
-        </MenuLink>
-      </Menu>
-    </Nav>
-  );
+			<Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
+				<MenuLink to="/">Home</MenuLink>
+				<MenuLink to="about">About</MenuLink>
+				<MenuLink to="register">Register</MenuLink>
+				<MenuLink to="login" onClick={() => sessionStorage.clear()}>
+					Logout
+				</MenuLink>
+			</Menu>
+		</Nav>
+	);
 };
 
 export default Navbar;
