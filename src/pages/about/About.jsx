9051461c82c10ea19684a137b16badcef676e8b7
omersb <omersaidbuldukk@gmail.com>
2023-01-01 12:09:53 +0300

    update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,19 +8,19 @@ const About = () => {
 			<StyledImage src={codingSvg} />
 			<div>
 				<h1>
-					About Software Developer <span>Josh Smith</span>
+					About Software Developer <span>Ömer Said Bulduk</span>
 				</h1>
 			</div>
 			<InfoContainer>
-				<h2>Hi, I'am Josh</h2>
+				<h2>Hi, I'am Ömer</h2>
 				<h3>I’m currently learning Full-Stack Development Languages.</h3>
 				<h4>
 					I've already known JS, ReactJS, ReactNative, NodeJS, MongoDB,SQL,
 					Python, AWS Services.
 				</h4>
 				<h2>
-					<a href="mailto:josh.smith@clarusway.com">Send email</a> :
-					josh.smith@clarusway.com
+					<a href="mailto:omersaidbuldukk@gmail.com">Send email</a> :
+					omersaidbuldukk@gmail.com
 				</h2>
 			</InfoContainer>
 		</AboutContainer>
